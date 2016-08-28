b933ae4172c6e943fb516235070034280722b00c
Carroll Vance <vancecs@gmail.com>
2016-08-28 14:59:00 -0500

    Updating default config file

--- config-example.py
+++ config-example.py
@@ -4,6 +4,7 @@ CONFIG_SERVE_DIR = "serve"
 
 CONFIG_GOOGLE_CX = "40230423040234:324234234"
 CONFIG_GOOGLE_KEY = "sdfSFDSFfDSfsdf_ASdaSDsd"
+CONFIG_GOOGLE_SAFESEARCH = "medium"
 
 CONFIG_DISCORD_TOKEN = "asdASDASDasdaSDasd.aSDASDasD.ASDasdASdASD"
 CONFIG_DISCORD_OWNER = "BigBoss#8527"
@@ -19,7 +20,7 @@ CONFIG_MARKOV_TICK_RATING_DEFAULT = 7
 CONFIG_MARKOV_TICK_RATING_DAILY_REDUCE = 1
 CONFIG_MARKOV_TOPIC_FILTER = ['nick', 'and', 'for', 'what', 'where', 'when', 'how', 'who', 'now', 'be', 'is', 'or',
                               'the', 'you', 'your', 'we', 'i', 'to', 'too', 'are', 'do', 'don\'t', 'what\'s', 'whats',
-                              'just', 'with', 'its', 'his', 'her', 'it', 'it\'s', 'that', 'thats','my']
+                              'just', 'with', 'its', 'his', 'her', 'it', 'it\'s', 'that', 'thats', 'my']
 CONFIG_MARKOV_TOPIC_WORD_MIN_LENGTH = 3
 CONFIG_MARKOV_CHAIN_ATTEMPTS = 10
 CONFIG_MARKOV_VECTOR_LENGTH = 7
