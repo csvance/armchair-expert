779592fa4271528a2662e517f34971a00baf8e66
Carroll Vance <vancecs@gmail.com>
2016-10-16 15:56:34 -0500

    Number of dice in !xdx commands now optional

--- extras.py
+++ extras.py
@@ -14,6 +14,9 @@ def roll_dice_cmd(cmd):
     sides = None
     plus = 0
 
+    if args[0] == '':
+        args[0] = '1'
+        
     try:
         dice = int(args[0])
         if(args[1].find("+") != -1):
@@ -30,7 +33,8 @@ def roll_dice_cmd(cmd):
 
 def command_router(cmd):
 
-    if(re.match("!\d{0,1}d\d{0,10}",cmd)):
+    if re.match("!\d{0,1}d\d{0,10}",cmd):
         return roll_dice_cmd(cmd)
-
+    elif re.match("!d\d{0,10}",cmd):
+        return roll_dice_cmd(cmd)
     return "No such command!"
