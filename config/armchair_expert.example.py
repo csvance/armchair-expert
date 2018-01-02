eab6dd1d7e1f1a085c8d557f0366b3bc477b1a0d
Carroll Vance <vancecs@gmail.com>
2018-01-01 18:09:34 -0600

    Fixing .gitignore, add missing config example files

---
+++ config/armchair_expert.example.py
@@ -0,0 +1,13 @@
+import logging
+
+# --- "User" Stuff Section ---
+# ----------------------------
+
+# Set loglevel / verbosity
+ARMCHAIR_EXPERT_LOGLEVEL = logging.INFO
+
+# --- Technical Stuff Section ---
+# -------------------------------
+
+# Store training data here
+IMPORT_TRAINING_DB_PATH = 'db/import.db'
---
+++ config/discord.example.py
@@ -0,0 +1,34 @@
+class DiscordApiCredentials(object):
+    def __init__(self, token: str):
+        self.token = token
+
+
+# --- "User" Stuff Section ---
+# ----------------------------
+
+DISCORD_CLIENT_ID = 12345678901234567890
+DISCORD_TOKEN = ''
+
+DISCORD_CREDENTIALS = DiscordApiCredentials(token=DISCORD_TOKEN)
+
+DISCORD_USERNAME = 'SomeBot#1234'
+
+# Learn from all servers and channels
+DISCORD_LEARN_FROM_ALL = False
+
+# Don't learn from any of these channels
+DISCORD_LEARN_CHANNEL_EXCEPTIONS = []
+
+# Learn from direct messages
+DISCORD_LEARN_FROM_DIRECT_MESSAGE = False
+
+# Learn from user
+DISCORD_LEARN_FROM_USER = None
+
+# --- Technical Stuff Section ---
+# -------------------------------
+
+DISCORD_REMOVE_URL = True
+
+# Store training data here
+DISCORD_TRAINING_DB_PATH = 'db/discord.db'
