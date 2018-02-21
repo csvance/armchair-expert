06e12942aa70d1b3b25e1c026800e126edec7891
Carroll Vance <vancecs@gmail.com>
2018-02-21 06:35:24 -0600

    Add verbose option to import_text_file.py

--- scripts/import_text_file.py
+++ scripts/import_text_file.py
@@ -6,6 +6,8 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('datafile')
+    parser.add_argument('--verbose', help='Print out each line of data stored for training',
+                        action='store_true')
     args = parser.parse_args()
 
     data = open(args.datafile, 'r').read()
@@ -17,7 +19,8 @@ def main():
     for line_idx, line in enumerate(lines):
         if line_idx % 1000 == 0:
             print("Import: %f%%" % (line_idx / len(lines) * 100))
-
+        if args.verbose:
+            print(line)
         data_manager.store(line)
 
     data_manager.commit()
